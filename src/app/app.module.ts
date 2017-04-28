4906d86f79a00e4014c8c87884098b86672b48ea
Amit Goel <amit.goel@roomerpms.com>
2017-04-28 10:56:32 +0200

    first commit - added html for first page and first level components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './topNav/top-nav/top-nav.component';
+import { LeftPanelComponent } from './leftPanel/left-panel/left-panel.component';
+import { MainPanelComponent } from './mainPanel/main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    LeftPanelComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule,
