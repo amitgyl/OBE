ae8acadfa735aa9d3767d1d85fd650314186ad32
Amit Goel <amit.goel@roomerpms.com>
2017-04-28 14:28:45 +0200

    added all components and filled with html code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { TopNavComponent } from './topNav/top-nav/top-nav.component';
 import { LeftPanelComponent } from './leftPanel/left-panel/left-panel.component';
 import { MainPanelComponent } from './mainPanel/main-panel/main-panel.component';
-import { StayDatesComponent } from './leftPanel/stayDates/stay-dates/stay-dates.component';
-import { SelectDatesComponent } from './leftPanel/stayDates/selectDates/select-dates/select-dates.component';
-import { SpecifyRoomsAndGuestsComponent } from './leftPanel/stayDates/specifyRoomsAndGuests/specify-rooms-and-guests/specify-rooms-and-guests.component';
+import { StayDatesComponent } from './leftPanel/left-panel/stayDates/stay-dates/stay-dates.component';
+import { SelectDatesComponent } from './leftPanel/left-panel/stayDates/stay-dates/selectDates/select-dates/select-dates.component';
+import { SpecifyRoomsAndGuestsComponent } from './leftPanel/left-panel/stayDates/stay-dates/specifyRoomsAndGuests/specify-rooms-and-guests/specify-rooms-and-guests.component';
+import { PromoCodeComponent } from './leftPanel/left-panel/stayDates/stay-dates/promoCode/promo-code/promo-code.component';
+import { CancelBookingComponent } from './leftPanel/left-panel/cancelBooking/cancel-booking/cancel-booking.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { SpecifyRoomsAndGuestsComponent } from './leftPanel/stayDates/specifyRoo
     MainPanelComponent,
     StayDatesComponent,
     SelectDatesComponent,
-    SpecifyRoomsAndGuestsComponent
+    SpecifyRoomsAndGuestsComponent,
+    PromoCodeComponent,
+    CancelBookingComponent
   ],
   imports: [
     BrowserModule,
