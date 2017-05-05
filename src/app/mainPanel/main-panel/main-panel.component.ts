3a2459cf80f8610940194e4810ddfe963444360d
Amit Goel <amit.goel@roomerpms.com>
2017-05-05 12:27:32 +0200

    added functionality for dynamic number of weeks

--- src/app/mainPanel/main-panel/main-panel.component.ts
+++ src/app/mainPanel/main-panel/main-panel.component.ts
@@ -13,30 +13,29 @@ export class MainPanelComponent implements OnInit {
   arrNightStay: any[]
   month1Number: any;
   month2Number: any;
-  month1: string;
-  month2: string;
+  month1Name: string;
+  month2Name: string;
   year1: any;
   year2: any;
-  month: string[];
   arrWeeksThisMonth: any[];
   arrWeeksNextMonth: any[];
   arrThisMonth: any[];//array storing info about current month
   arrNextMonth: any[];//array storing info about next month
   arrAvailabilty: any[];
 
-  applyClass(a){
-   const isStay = a.isStaying 
-   const isArrive = a.isArrivalDate 
-   const isDepart =  a.isDeparture
-   const isDisabled =  a.disabled
-   const isNotAvailable = (a.availability < 1)    
-   return {
-     's-d': ((isStay || isArrive || isDepart) && !isDisabled), 
-     'arrival': (isArrive && !isDisabled), 
-     'departure': (isDepart && !isDisabled), 
-     'not-available': (isNotAvailable && !isDisabled), 
-     'n-a': (isNotAvailable && !isDisabled), 
-     'r-a': ((!(isStay || isArrive || isDepart) && a.day) && !isDisabled)
+  applyClass(a) {
+    const isStay = a.isStaying
+    const isArrive = a.isArrivalDate
+    const isDepart = a.isDeparture
+    const isDisabled = a.disabled
+    const isNotAvailable = (a.availability < 1)
+    return {
+      's-d': ((isStay || isArrive || isDepart) && !isDisabled),
+      'arrival': (isArrive && !isDisabled),
+      'departure': (isDepart && !isDisabled),
+      'not-available': (isNotAvailable && !isDisabled),
+      'n-a': (isNotAvailable && !isDisabled),
+      'r-a': ((!(isStay || isArrive || isDepart) && a.day) && !isDisabled)
     }
   }
 
@@ -46,121 +45,52 @@ export class MainPanelComponent implements OnInit {
 
   ngOnInit() {
     this.setStayNights()
-      
+    this.setMonthNameYear()
   }
 
   setStayNights() {
     this.availabilty = this.roomsAvailable.getAvailibility(this.arrivalDate, this.departureDate);
-    if (!this.arrivalDate){
-      this.arrivalDate = this.stringToDate("5/25/2017", "mm/dd/yyyy", "/")
-    }    
-    if (!this.departureDate){
-      this.departureDate = this.stringToDate("5/30/2017", "mm/dd/yyyy", "/")
-    }  
+    if (!this.arrivalDate) {
+      this.arrivalDate = new Date()
+      //this.arrivalDate = new Date(this.arrivalDate.getFullYear(),this.arrivalDate.getMonth(),this.arrivalDate.getDate())
+      this.arrivalDate = this.stringToDate(this.dateToString(this.arrivalDate),"mm/dd/yyyy", "/")
+      this.arrivalDate = this.stringToDate("5/25/2017", "mm/dd/yyyy", "/") //to be later changed to today (above)
+    }
+    
+    if (!this.departureDate) {
+      this.departureDate = this.stringToDate("5/30/2017", "mm/dd/yyyy", "/")//to be later changed to tomorrow (below)
+      //this.departureDate = new Date(this.arrivalDate.getTime() + (86400000))
+    }
     this.numberOfNights = (this.departureDate - this.arrivalDate) / (1000 * 60 * 60 * 24)
-    this.arrNightStay = new Array()
+    this.arrNightStay = new Array() //this array stores all the days excluding arrival and departure date
     for (var i = 0; i < this.numberOfNights - 1; i++) {
       this.arrNightStay[i] = new Date(this.arrivalDate.getTime() + ((i + 1) * (86400000)))
     }
-    this.setMonth()  
   }
 
-  setCalendar(month1Number,month2Number) {
-    this.arrThisMonth = new Array()
-    this.arrNextMonth = new Array()    
-
-    for (var i = 0; i < this.availabilty.availability.availabilityItem.length; i++) {
-      if (
-          month1Number == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getMonth()
-          && 
-          this.year1 == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getFullYear()
-         ) {
-        this.arrThisMonth.push(this.availabilty.availability.availabilityItem[i])
-      }
-      if (
-          month2Number == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getMonth()
-          &&
-          this.year2 == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getFullYear()          
-         ) {
-        this.arrNextMonth.push(this.availabilty.availability.availabilityItem[i])
-      }
-    }
-    
-    this.arrWeeksThisMonth = this.fillMonth(this.arrThisMonth,this.arrWeeksThisMonth)
-    this.arrWeeksNextMonth = this.fillMonth(this.arrNextMonth,this.arrWeeksNextMonth)    
-
-  }
-
-  fillMonth(arrCalMonth,arWeeks){
-    var dayNummer = 0
-    var dayNummer1 = 0
-    //dayNummer is for the td and dayNummer1 is for index of element in availbilty array
-    arWeeks = new Array();
-
-    for (var i = 1; i < 7; i++) {
-      var arDays = new Array();
-      for (var j = 1; j < 8; j++) {       
-        if ((dayNummer1<arrCalMonth.length) && (dayNummer%7 == this.stringToDate(arrCalMonth[dayNummer1].date,"mm/dd/yyyy","/").getDay()) ){          
-          arrCalMonth[dayNummer1].day = this.stringToDate(arrCalMonth[dayNummer1].date,"mm/dd/yyyy","/").getDate()
-          
-          if (this.stringToDate(arrCalMonth[dayNummer1].date,"mm/dd/yyyy","/").getTime() == this.arrivalDate.getTime()){
-            arrCalMonth[dayNummer1].isArrivalDate = true
-          }
-          
-          if (this.stringToDate(arrCalMonth[dayNummer1].date,"mm/dd/yyyy","/").getTime() == this.departureDate.getTime()){
-            arrCalMonth[dayNummer1].isDeparture = true
-          }
-          if (dayNummer1>0){
-            if (arrCalMonth[dayNummer1].availability<1){
-              arrCalMonth[dayNummer1-1].closedForDeparture = true                    
-            }
-          }
-          var d = new Date()
-          if (this.stringToDate(arrCalMonth[dayNummer1].date, "mm/dd/yyyy", "/").getTime() < d.getTime()) {
-              arrCalMonth[dayNummer1].disabled = true
-          }
-          for (var k = 0; k < this.arrNightStay.length; k++) {
-            if (this.arrNightStay[k].getTime() == this.stringToDate(arrCalMonth[dayNummer1].date, "mm/dd/yyyy", "/").getTime()) {
-              arrCalMonth[dayNummer1].isStaying = true
-            }           
-          }          
-          arDays.push(arrCalMonth[dayNummer1])
-          dayNummer1++
-        } 
-        else{
-          var x = new Array()
-          arDays.push(x)
-        }                
-        dayNummer++
-      }
-      arWeeks.push(arDays)
-    }    
-    return arWeeks
-  }
-
-  setMonth() {
-    this.month = new Array();
-    this.month[0] = "January";
-    this.month[1] = "February";
-    this.month[2] = "March";
-    this.month[3] = "April";
-    this.month[4] = "May";
-    this.month[5] = "June";
-    this.month[6] = "July";
-    this.month[7] = "August";
-    this.month[8] = "September";
-    this.month[9] = "October";
-    this.month[10] = "November";
-    this.month[11] = "December";
-    this.month[12] = "January";
+  setMonthNameYear() {
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    //month[12] = "January";
 
     if (this.month1Number == undefined) {
       var d = new Date();
       this.month1Number = d.getMonth();
       this.year1 = d.getFullYear();
     }
-    this.month1 = this.month[this.month1Number];
-    
+    this.month1Name = month[this.month1Number]; //gets monthName from month number
+
     if (this.month1Number == 11) {
       this.year2 = this.year1 + 1
       this.month2Number = 0
@@ -169,26 +99,126 @@ export class MainPanelComponent implements OnInit {
       this.year2 = this.year1;
       this.month2Number = this.month1Number + 1
     }
-    this.month2 = this.month[this.month2Number];
+    this.month2Name = month[this.month2Number];
     this.setCalendar(this.month1Number, this.month2Number)
 
   }
-  showSpecificMonth(){
-    var selectedDateFromLeftPanel = "8/26/2017"
-    this.arrivalDate =  this.stringToDate(selectedDateFromLeftPanel, "mm/dd/yyyy", "/")
-    this.departureDate = new Date(this.arrivalDate.getTime() + (86400000))          
-    this.month1Number = this.stringToDate(selectedDateFromLeftPanel, "mm/dd/yyyy", "/").getMonth()
-    this.setStayNights();
+
+  setCalendar(month1Number, month2Number) {
+    this.arrThisMonth = new Array()
+    this.arrNextMonth = new Array()
+
+    for (var i = 0; i < this.availabilty.availability.availabilityItem.length; i++) {
+      if (
+        month1Number == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getMonth()
+        &&
+        this.year1 == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getFullYear()
+      ) {
+        this.arrThisMonth.push(this.availabilty.availability.availabilityItem[i])
+      }
+      else if (
+        month2Number == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getMonth()
+        &&
+        this.year2 == this.stringToDate(this.availabilty.availability.availabilityItem[i].date, "mm/dd/yyyy", "/").getFullYear()
+      ) {
+        this.arrNextMonth.push(this.availabilty.availability.availabilityItem[i])
+      }
+    }
+
+    this.arrWeeksThisMonth = this.fillMonth(this.arrThisMonth)
+    this.arrWeeksNextMonth = this.fillMonth(this.arrNextMonth)
+
   }
+
+  fillMonth(arrCalMonth) {
+    var dayNummer = 0
+    var dayNummer1 = 0
+    //dayNummer is for the td and dayNummer1 is for index of element in availbilty array
+    var arWeeks = new Array();
+
+    var numberOfWeeks = 7
+    console.log(this.stringToDate(arrCalMonth[0].date, "mm/dd/yyyy", "/").getDay())
+    if(arrCalMonth.length == 31){      
+      if (this.stringToDate(arrCalMonth[0].date, "mm/dd/yyyy", "/").getDay()< 5){
+        numberOfWeeks = 6
+      }
+    }
+    else {      
+      if (this.stringToDate(arrCalMonth[0].date, "mm/dd/yyyy", "/").getDay()< 6){
+        numberOfWeeks = 6
+      }
+    }
+
+    for (var i = 1; i <numberOfWeeks; i++) {
+      var arrDays = new Array();
+      for (var j = 1; j < 8; j++) {
+        if ((dayNummer1 < arrCalMonth.length) && (dayNummer % 7 == this.stringToDate(arrCalMonth[dayNummer1].date, "mm/dd/yyyy", "/").getDay())) {
+          arrCalMonth[dayNummer1].day = this.stringToDate(arrCalMonth[dayNummer1].date, "mm/dd/yyyy", "/").getDate()
+
+          
+          if (arrCalMonth[dayNummer1].date == this.dateToString(this.arrivalDate)) {
+            arrCalMonth[dayNummer1].isArrivalDate = true
+          }
+
+          else if (arrCalMonth[dayNummer1].date == this.dateToString(this.departureDate)) {         
+            arrCalMonth[dayNummer1].isDeparture = true
+          }
+
+          //logic for applyClass start
+
+          // if (dayNummer1 > 0) {
+          //   if (arrCalMonth[dayNummer1].availability < 1) {
+          //     arrCalMonth[dayNummer1 - 1].closedForDeparture = true
+          //   }
+          // }
+
+          //disable past dates          
+          var d = new Date()
+          if (this.stringToDate(arrCalMonth[dayNummer1].date, "mm/dd/yyyy", "/") < this.stringToDate(this.dateToString(d),"mm/dd/yyyy", "/")) {
+            arrCalMonth[dayNummer1].disabled = true
+          }
+          if (arrCalMonth[dayNummer1].date == this.dateToString(this.arrivalDate)) {
+          }
+          
+          for (var k = 0; k < this.arrNightStay.length; k++) {
+            if (this.dateToString(this.arrNightStay[k]) == arrCalMonth[dayNummer1].date) {
+              arrCalMonth[dayNummer1].isStaying = true
+            }
+          }
+          //logic for applyClass end
+          arrDays.push(arrCalMonth[dayNummer1])
+          dayNummer1++
+        }
+        else {
+          var x = new Array()
+          arrDays.push(x)
+        }
+        dayNummer++
+      }
+      arWeeks.push(arrDays)
+    }
+    return arWeeks
+  }
+
+  
+  showSpecificMonth() {
+    var selectedDateFromLeftPanel = "8/26/2017"
+    this.arrivalDate = this.stringToDate(selectedDateFromLeftPanel, "mm/dd/yyyy", "/")
+    this.departureDate = new Date(this.arrivalDate.getTime() + (86400000))
+    this.month1Number = this.stringToDate(selectedDateFromLeftPanel, "mm/dd/yyyy", "/").getMonth()
+    this.setStayNights()
+    this.setMonthNameYear()
+  }
+
   showNextMonth() {
     if (this.month1Number == 11) {
-      this.month1Number = 0;
-      this.year1++;
+      this.month1Number = 0
+      this.year1++
     }
     else {
       this.month1Number++;
     }
-    this.setMonth();
+    this.setMonthNameYear();
   }
 
   showPreviousMonth() {
@@ -206,7 +236,7 @@ export class MainPanelComponent implements OnInit {
       else {
         this.month1Number--;
       }
-      this.setMonth();
+      this.setMonthNameYear();
     }
   }
 
@@ -222,5 +252,9 @@ export class MainPanelComponent implements OnInit {
     month -= 1;
     var formatedDate = new Date(dateItems[yearIndex], month, dateItems[dayIndex]);
     return formatedDate;
+  }
+
+  dateToString(dateObj){
+    return (1+dateObj.getMonth())+"/"+dateObj.getDate()+"/"+dateObj.getFullYear()
   }
 }
